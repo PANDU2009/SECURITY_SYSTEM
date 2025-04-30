b09c3d70cfda4399248b43c5ff7dfaddc00d2294
PANDU2009 <138602704+PANDU2009@users.noreply.github.com>
2025-05-01 00:59:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function myFunction() {
+  var x = document.getElementById("pass");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function readInput() {
+  var username = document.getElementById("user").value;
+
+  var password = document.getElementById("pass").value;
+  event.preventDefault();
+  if ((username=="PANDU"||username=="pandu"||username=="Pandu" )&& password=="1234"){
+    window.location.href=" main.html";
+  }
+  else{
+    alert("Invalid crediantials");
+    window.location.href="index.html";
+  }
+  // You can now use the userInput variable as needed
+}
+function returnToLogin() {
+  window.location.href = "index.html";
+}
+
